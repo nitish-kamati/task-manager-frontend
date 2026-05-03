446993c8cea87e14df1d06faaeca5681a104e7fa
nitish-kamati <ntsh02kamati@gmail.com>
2026-05-04 02:23:07 +0530

    Add deployment configuration for Vercel

--- src/api/axiosInstance.js
+++ src/api/axiosInstance.js
@@ -2,7 +2,7 @@ import axios from "axios";
 import { clearAuth, getToken } from "../utils/auth.js";
 
 const api = axios.create({
-  baseURL: "http://localhost:8080",
+  baseURL: import.meta.env.VITE_API_BASE_URL || "http://localhost:8080",
   headers: {
     "Content-Type": "application/json"
   }
